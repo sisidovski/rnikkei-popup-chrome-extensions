fa1f33a3f228ade4eeee81036753ac6cb24af52c
sisidovski <arerreee@gmail.com>
2018-03-19 16:53:34 +0900

    Fix empty url issue

--- background.js
+++ background.js
@@ -1,11 +1,17 @@
-let url = '';
+const getRNikkeiURL = wwwURL => {
+  const url = new URL(wwwURL);
+  url.host = 'r.nikkei.com';
+  url.search = '';
+  return url.toString();
+};
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-  url = request.url;
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   chrome.pageAction.show(sender.tab.id);
   sendResponse({tabid: sender.tab.id});
 });
 
 chrome.pageAction.onClicked.addListener(function(tab) {
-  window.open(url, '', 'width=350, height=600');
+  chrome.tabs.getSelected(null, activeTab => {
+    window.open(getRNikkeiURL(activeTab.url), '', 'width=350, height=600');
+  });
 });
--- content_script.js
+++ content_script.js
@@ -1,5 +1,1 @@
-const url = new URL(location.href);
-url.host = 'r.nikkei.com';
-url.search = '';
-
-chrome.runtime.sendMessage({url: url.toString()}, function(response) {});
+chrome.runtime.sendMessage({}, function(response) {});
